3795ebc47d22dd0adb4e3449ddd4556ba4542997
Seerean <chrisnnchris@gmail.com>
2017-12-21 20:02:51 -0800

    Add files via upload
    Updated scripts to compute simple moving average. Not complete yet. Will revise later.

--- barometerScripts.js
+++ barometerScripts.js
@@ -1,9 +1,3 @@
-/* Streaks are important for swing traders because 4 green days intrinsically means a red day is coming.
-In my sheet, each row has a cell that outputs 1 if green day or -1 if red day.
-Streak takes an array of those cells and outputs the streak. 
-If streak is 4, but then has a red day, it comes -1.
-If streak is -3, but then has a green day, it comes a 1.
-*/
 function streak(input) {
   if (input.map) {            // Test whether input is an array.
     var streakSum = 0;
@@ -20,3 +14,26 @@ function streak(input) {
     return "You didn't select a group of cells. Check your input";
   }
 }
+
+function grabCurrent50SMA(input, input2) {
+  var sum = 0;
+  if (input.map) {
+    for (i = 0; i < input.length; i++) {
+      sum += parseInt(input[i]);
+    }  
+    sum += input2;
+    return sum / 50;
+  } else {
+    return "You did not return 50 dates";
+  }  
+}
+function grabPrevious50SMA() {
+}  
+function grabCurrent100SMA() {
+}  
+function grabPrevious100SMA() {
+}
+function grabCurrent200SMA() {
+}  
+function grabPrevious200SMA() {
+}  
