1bcd67d417807684d766f1fbbf1c0aec99901604
Seerean <chrisnnchris@gmail.com>
2018-01-08 23:01:08 -0800

    Finally figured out how to calculate SMA.
    Parsefloat is the magic tool.

--- barometerScripts.js
+++ barometerScripts.js
@@ -1,5 +1,5 @@
 function streak(input) {
-  if (input.map) {            // Test whether input is an array.
+  if (input.map) {            
     var streakSum = 0;
     for (i = 0; i < input.length; i++) {
       //streakSum += parseInt(input[i]);
@@ -17,23 +17,31 @@ function streak(input) {
 
 function grabCurrent50SMA(input, input2) {
   var sum = 0;
-  if (input.map) {
+  if (input.map) {  
     for (i = 0; i < input.length; i++) {
-      sum += parseInt(input[i]);
+      sum += parseFloat(input[i]);
+    }
+    sum += parseFloat(input2);
+    return sum/50;
+    /* JUNK CODE, but perhaps useful legacy
+    for (i = 0; i < input.length; i++) {
+      //sum += parseFloat(input[i]);
+      if (i == 49) {
+        return "what?";
+        return parseFloat(input[i]);
+      }
     }  
-    sum += input2;
-    return sum / 50;
+    sum += input.length;
+    */
+    //sum += parseFloat(input2);
   } else {
     return "You did not return 50 dates";
   }  
 }
-function grabPrevious50SMA() {
-}  
+
 function grabCurrent100SMA() {
 }  
-function grabPrevious100SMA() {
-}
+
 function grabCurrent200SMA() {
 }  
-function grabPrevious200SMA() {
-}  
+  
